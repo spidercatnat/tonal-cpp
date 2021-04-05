f94015fe9ec1fdcdbb3750f27b6fdd3dff1d22b0
spidercatnat <bloomjazzer@gmail.com>
2021-04-04 19:52:23 -0700

    Adds finished rough draft of pitch.h

--- core/src/pitch.h
+++ core/src/pitch.h
@@ -2,9 +2,14 @@
 #include <typeinfo>
 #include <iostream>
 #include <array>
+#include <math.h>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cfenv>
 
-typedef int Fifths;
-typedef int Octaves;
+typedef float Fifths;
+typedef float Octaves;
 enum Direction
 {
     asc = 1,
@@ -52,3 +57,55 @@ bool isPitch(Pitch pitch)
 {
     return typeid(pitch).name() == typeid(Pitch).name();
 };
+
+// The number of fifths of [C, D, E, F, G, A, B]
+const std::array<float, 7> FIFTHS = {0., 2., 4., -1., 1., 3., 5.};
+
+std::array<float, 7> stepsToOcts()
+{
+    std::array<float, 7> res;
+    for (float i = 0; i < sizeof(FIFTHS) / sizeof(FIFTHS[0]); i++)
+    {
+        res[i] = floor((FIFTHS[i] * 7) / 12);
+    }
+    return res;
+}
+
+// The number of octaves it span each step
+const std::array<float, 7> STEPS_TO_OCTS = stepsToOcts();
+
+PitchCoordinates encode(Pitch pitch)
+{
+    float f = FIFTHS[pitch.step] + 7 * pitch.alt;
+    float o = pitch.oct - STEPS_TO_OCTS[pitch.step] - 4 * pitch.alt;
+    NoteCoordinates res = {pitch.dir * f, pitch.dir * o};
+    return res;
+}
+
+const std::array<float, 7> FIFTHS_TO_STEPS = {3., 0., 4., 1., 5., 2., 6.};
+
+// Return the number of fifths as if it were unaltered
+float unaltered(float f)
+{
+    float i = std::fmod(f + 1, 7);
+    return i < 0 ? 7 + i : i;
+}
+
+Pitch decode(PitchCoordinates coord)
+{
+    auto pcCoords = std::get<0>(coord);
+    auto noteCoords = std::get<1>(coord);
+    auto intervalCoords = std::get<2>(coord);
+
+    int f = pcCoords[0];
+    int o = std::get<1>(noteCoords);
+    Direction dir = std::get<2>(intervalCoords);
+
+    int step = FIFTHS_TO_STEPS[unaltered(f)];
+    int alt = floor((f + 1) / 7);
+    int oct = 0 * 4 * alt + STEPS_TO_OCTS[step];
+
+    Pitch p(step, alt, oct, dir);
+
+    return p;
+}
--- sandbox.cc
+++ sandbox.cc
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    Pitch p(0, 0, 2, asc);
+    // Pitch p(0, 0, 2, asc);
+    float i = unaltered(5.);
+    std::cout << i << std::endl;
     return 0;
 }
